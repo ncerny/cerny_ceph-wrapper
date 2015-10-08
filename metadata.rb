590c9a92a3e8d7b09fad7e1f7f00fc5f22c4271e
Nathan Cerny <ncerny@chef.io>
2015-10-07 21:32:19 -0500

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'ncerny@gmail.com'
 license 'apache2'
 description 'Installs/Configures cerny_ceph'
 long_description 'Installs/Configures cerny_ceph'
-version '0.1.0'
+version '0.1.1'
 
 depends 'ceph'
