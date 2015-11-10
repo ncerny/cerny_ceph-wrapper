b89e835011b89aa6a3a79338e98bf8581041f56b
Nathan Cerny <ncerny@gmail.com>
2015-11-10 14:45:07 -0600

    Bump 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'ncerny@gmail.com'
 license 'apache2'
 description 'Installs/Configures cerny_ceph'
 long_description 'Installs/Configures cerny_ceph'
-version '0.1.1'
+version '0.2.0'
 
 depends 'ceph'
