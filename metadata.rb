fc125f46c17e02261eaa045c96e657561011369d
Nathan Cerny <ncerny@gmail.com>
2015-11-10 17:24:07 -0600

    Remove apache recipe

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'ncerny@gmail.com'
 license 'apache2'
 description 'Installs/Configures cerny_ceph'
 long_description 'Installs/Configures cerny_ceph'
-version '0.2.5'
+version '0.2.6'
 
 depends 'ceph'
--- recipes/radosgw.rb
+++ recipes/radosgw.rb
@@ -16,5 +16,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include_recipe 'ceph::radosgw_apache2'
 include_recipe 'ceph::radosgw'
