7a25da06a8265b35bf5c89d3d1891778f6ab49d4
Nathan Cerny <ncerny@gmail.com>
2015-11-10 17:19:28 -0600

    Switch to civetweb

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'ncerny@gmail.com'
 license 'apache2'
 description 'Installs/Configures cerny_ceph'
 long_description 'Installs/Configures cerny_ceph'
-version '0.2.4'
+version '0.2.5'
 
 depends 'ceph'
