63cbd5429a97599d74c46003c32fb84f16c03ed1
Nathan Cerny <ncerny@chef.io>
2015-12-10 16:46:10 -0600

    Add default configuration tuning

--- attributes/ceph.rb
+++
@@ -1,7 +0,0 @@
-default['ceph']['version'] = 'hammer'
-default['ceph']['rhel']['stable']['repository'] = 'http://ceph.com/rpm-hammer/el7/x86_64/'
-default['ceph']['radosgw']['api_fqdn'] = 'ceph.internal.cerny.cc'
-default['ceph']['radosgw']['admin_email'] = 'ncerny@gmail.com'
-default['ceph']['radosgw']['rgw_addr'] = '*:80'
-default['ceph']['radosgw']['rgw_port'] = false
-default['ceph']['radosgw']['webserver_companion'] = 'civetweb'
